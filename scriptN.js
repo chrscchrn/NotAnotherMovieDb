17e8ddc53522d2d7b379b018ee79829a8f5eb2a4
Nolan Terpstra <nbakert@gmail.com>
2020-08-18 11:14:45 -0500

    Generating the different genres in the dropdown menu.

---
+++ scriptN.js
@@ -0,0 +1,14 @@
+const genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War", "Western"];
+
+for (let i = 0; i < genres.length; i++) {
+    newGenre = $("<option>");
+    newGenre.text(genres[i]);
+    newGenre.data("name", genres[i]);
+    $("#dropdown1").append(newGenre);
+}
+
+$('.dropdown-trigger').dropdown();
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
