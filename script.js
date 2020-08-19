bf794bc22893c7b1420151da87ae4311a600bf5f
Calum Held <calumheld@gmail.com>
2020-08-19 14:34:10 -0500

    making sure the issue with git is solved

--- script.js
+++ script.js
@@ -87,16 +87,5 @@ $(document).ready(function () {
         });
         }
 
-    // var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=28"
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // })
-    //     .then(function (response) {
-
-    //         console.log(queryURL);
-    //         console.log(response);
-
-    //     });
 })
 
