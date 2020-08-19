e7afd525f38c0ebb3da38d30f4abdd3c273a49c4
chrscchrn <cochranschris@gmail.com>
2020-08-19 10:15:07 -0500

    canvas animation experiment

--- script.js
+++ script.js
@@ -30,14 +30,18 @@ $(document).ready(function () {
         $("#genreDropDown").append(newGenre);
     }
 
+
     $('select').formSelect();
 
     $("#submit").on("click", function(event){
         event.preventDefault();
         genreID= $("#genreDropDown").val();
         console.log(genreID);
-    })
+       
 
+        
+    })
+    
     var APIKey = "5a3f3373b8ebcad2db18450af15ec4fd";
 
     var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=5a3f3373b8ebcad2db18450af15ec4fd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=28"
@@ -52,3 +56,4 @@ $(document).ready(function () {
 
         });
 })
+
